16a711716e41a17f5f4ed56674921fd6e73550d8
Pdawg-bytes <pdawg11239linusfan@gmail.com>
2022-08-22 23:36:36 -0400

    Source Upload

---
+++ Microsoft Band Simulator/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Microsoft_Band_Simulator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Inits frame and navigates to it in MainWindow
+            Band2Frame.Navigate(typeof(Band2), null);
+            pwon.IsEnabled = false;
+        }
+
+        private void OffButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Will hide frame and re-enable pwon button
+            pwon.IsEnabled = true;
+        }
+    }
+}
